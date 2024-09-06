2bdbe5d5e1b971a736590db096baaddb6a86435d
Gabriel Grinberg <gabi.grinberg@gmail.com>
2024-09-06 21:48:31 +0300

    fix

--- flow-editor/src/NodePreview/NodePreview.tsx
+++ flow-editor/src/NodePreview/NodePreview.tsx
@@ -31,7 +31,6 @@ export const NodePreview: React.FC<{ node: NodeDefinition }> = ({ node }) => {
     onChangeVisibleInputs: noop,
     onChangeVisibleOutputs: noop,
     forceShowMinimized: "both",
-    onExtractInlineNode: noop as any,
     onCloseInlineEditor: noop,
     isConnectedInstanceSelected: false,
     inlineEditorPortalDomNode: null as any,
