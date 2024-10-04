48de04b8f59f5d6bf5c00571468bf1a0a7de1db0
Gabriel Grinberg <gabi.grinberg@gmail.com>
2024-10-04 10:55:21 +0300

    mock api call

--- website/src/pages/_hero-example/HeroExample.spec.ts
+++ website/src/pages/_hero-example/HeroExample.spec.ts
@@ -48,12 +48,23 @@ describe("Hero examples", () => {
   }).timeout(6000);
 
   it("runs http example", async () => {
+    // Mock the first API call to get the country
     nock("https://api.country.is").get("/").reply(200, { country: "IL" });
+
+    // Mock the second API call to get the capital
+    nock("https://countriesnow.space")
+      .post("/api/v0.1/countries/capital", { iso2: "IL" })
+      .reply(200, {
+        data: {
+          name: "Denmark",
+          capital: "Copenhagen",
+        },
+      });
 
     const execute = await loadFlow("./ExampleHTTPRequests.flyde", __dirname);
 
     const { output } = await execute().result;
 
-    assert.deepEqual(output, "You're in Israel, whose capital is Jerusalem");
+    assert.deepEqual(output, "You're in Denmark, whose capital is Copenhagen");
   }).timeout(6000);
 });
