afcc200306424279b5f5b3a3777afc8ffd403dd6
Roshan Jagadish <roshan10.rj@gmail.com>
2025-04-14 19:01:19 +0530

    fix: Broken Inspect Pin by Hover

--- flow-editor/src/visual-node-editor/VisualNodeEditor.tsx
+++ flow-editor/src/visual-node-editor/VisualNodeEditor.tsx
@@ -1378,6 +1378,7 @@ export const VisualNodeEditor: React.FC<VisualNodeEditorProps & { ref?: any }> =
             <ContextMenuTrigger
               className={classNames("visual-node-editor", props.className, {
                 dark: darkMode,
+                dragged: draggedConnection !== null,
               })}
               data-id={node.id}
               disabled={!isBoardInFocus.current}
